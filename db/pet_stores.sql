b3ef210647f21b35ed17f5575fa6476c61edfd0c
katemanson <katemanson107@hotmail.com>
2016-10-12 22:27:23 +0100

    adds pet find_by_name method

--- db/pet_stores.sql
+++ db/pet_stores.sql
@@ -2,15 +2,15 @@ DROP TABLE IF EXISTS pets;
 DROP TABLE IF EXISTS pet_stores;
 
 CREATE TABLE pet_stores(
-id SERIAL4 PRIMARY KEY, 
-name VARCHAR(255),
-address VARCHAR(255),
-stock_type VARCHAR(255)
+  id SERIAL4 PRIMARY KEY, 
+  name VARCHAR(255),
+  address VARCHAR(255),
+  stock_type VARCHAR(255)
 );
 
 CREATE TABLE pets(
-id SERIAL4 PRIMARY KEY,
-name VARCHAR(255),
-type VARCHAR(255),
-store_id INT4 REFERENCES pet_stores(id)
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  store_id INT4 REFERENCES pet_stores(id)
 );
--- models/pet.rb
+++ models/pet.rb
@@ -14,7 +14,7 @@ class Pet
 
   def save
     sql = "INSERT INTO pets (name, type, store_id) VALUES ( '#{@name}', '#{@type}', #{@store_id} ) RETURNING *"
-    pet = SqlRunner.run( sql ).first
+    pet = SqlRunner.run(sql).first
     @id = pet['id']
   end
 
@@ -24,10 +24,32 @@ class Pet
     result = PetStore.new(store)
   end
 
-  def self.find(id)
+  def self.find_by_id(id)
     sql = "SELECT * FROM pets WHERE id = #{id}" 
     pet = SqlRunner.run(sql).first
     return Pet.new(pet)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM pets WHERE name = '#{name}'" #?As it stands, have to enter argument (name) within single quotes. Any way round this? 
+    pet = SqlRunner.run(sql).first
+    return Pet.new(pet)
+  end
+
+  def self.mash(id_1, id_2)
+    pet_1 = self.find_by_id(id_1)
+    pet_2 = self.find_by_id(id_2)
+
+    new_name_beginning = pet_1.name[0..2]
+    new_name_end = pet_2.name[-3..-1]
+    new_name = new_name_beginning + new_name_end
+
+    new_type_beginning = pet_1.type[0..3]
+    new_type_end = pet_2.type[-4..-1]
+    new_type = new_type_beginning + new_type_end
+
+    new_pet = Pet.new({'name' => "#{new_name}", 'type' => "#{new_type}", 'store_id' => pet_1.store_id})
+    new_pet.save 
   end
 
   def self.list
